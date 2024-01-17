0df707d1f94875a58a4b7c8076a788d53e51b46f
haval95 <havalrezan22@gmail.com>
2024-01-18 00:02:01 +0100

    hero menu button fixed

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Hero() {
@@ -7,7 +8,7 @@ function Hero() {
                 <div className="text-white text-center p-5   shadow-lg  rounded-lg">
                     <h1 className=" text-5xl text-white ">  Special Food<span className="text-yellow-500">.</span></h1>
                     <h2 className="mb-3 text-end">Passionate Creations! </h2>
-                    <button className='btn text-white border-none bg-yellow-500 rounded-full px-5 py-1 shadow-md hover:shadow-none hover:bg-yellow-600 duration-500'>Check Menu</button>
+                    <Link href="/menu" className='btn text-white border-none bg-yellow-500 rounded-full px-5 py-1 shadow-md hover:shadow-none hover:bg-yellow-600 duration-500'>Check Menu</Link>
                 </div>
               </div>
     </div>
