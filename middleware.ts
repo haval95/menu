71dd623baa89dd8a1cf048b12da2020dc727f672
haval95 <havalrezan22@gmail.com>
2024-01-29 14:21:36 +0100

    publick route re-define

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 export default authMiddleware({
-  publicRoutes: ["/", "/about", "/api", "/menu", "/menu/(.*)", "/sign-in"],
+  publicRoutes: ["/",],
 });
 
 export const config = {
