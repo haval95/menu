b017280b61d3a63a4162a2fb1321601753bcf56c
haval95 <havalrezan22@gmail.com>
2024-01-31 09:55:00 +0100

    categories responsiveness

--- app/menu/_components/CategoriesList.tsx
+++ app/menu/_components/CategoriesList.tsx
@@ -11,8 +11,8 @@ export default async function  CategoriesList() {
 
 
   const content = (
-    <div className="w-screen  flex justify-center">
-    <div className="flex 2xl:mx-14  w-full  max-w-screen-2xl  justify-center items-center flex-wrap gap-14 mt-14 mb-14  ">
+    <div className="  flex justify-center">
+    <div className="flex  w-full max-w-screen-2xl 2xl:mx-14 justify-center items-center flex-wrap gap-14 mt-14 mb-14  ">
       {
         categories.map(eachCategory => {
           return (<CategoryCard category={eachCategory} key={eachCategory.id} />)
